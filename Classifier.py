45c4e055896ab52ebc0874c83162fe07702839ac
Evan Schlesinger <esslushy@gmail.com>
2022-02-17 14:45:46 -0500

    finished run section of the model

--- Classifier.py
+++ Classifier.py
@@ -9,46 +9,49 @@ from model import build_model
 AUTOTUNE = tf.data.AUTOTUNE
 tf.random.set_seed(SEED)
 
-# Dataset loading
-print('Loading Data')
-# Loading train dataset from its directory batched to batch size, with 20% saved for validation
-print('Loading training portion of the training data')
-raw_train_dataset = tf.keras.utils.text_dataset_from_directory(
-    './dataset/aclImdb/train',
-    batch_size=BATCH_SIZE,
-    validation_split=0.2,
-    subset='training',
-    seed=SEED
-)
-# Class names for our data points
-class_names = raw_train_dataset.class_names
-# Set it up so that it runs faster while we need to access it
-train_dataset = raw_train_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-print('Finished loading training portion of the training data')
+def load_data():
+    # Dataset loading
+    print('Loading Data')
+    # Loading train dataset from its directory batched to batch size, with 20% saved for validation
+    print('Loading training portion of the training data')
+    raw_train_dataset = tf.keras.utils.text_dataset_from_directory(
+        './dataset/aclImdb/train',
+        batch_size=BATCH_SIZE,
+        validation_split=0.2,
+        subset='training',
+        seed=SEED
+    )
+    # Class names for our data points
+    class_names = raw_train_dataset.class_names
+    # Set it up so that it runs faster while we need to access it
+    train_dataset = raw_train_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    print('Finished loading training portion of the training data')
 
-print('Loading validation protion of the training data')
-# Loading validation dataset from its directory batched to batch size using the 20% saved for validation
-raw_validation_dataset = tf.keras.utils.text_dataset_from_directory(
-    './dataset/aclImdb/train',
-    batch_size=BATCH_SIZE,
-    validation_split=0.2,
-    subset='validation',
-    seed=SEED
-)
-# Set it up so that it runs faster while we need to access it
-validation_dataset = raw_validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-print('Finished loading validation portion of the training data')
+    print('Loading validation protion of the training data')
+    # Loading validation dataset from its directory batched to batch size using the 20% saved for validation
+    raw_validation_dataset = tf.keras.utils.text_dataset_from_directory(
+        './dataset/aclImdb/train',
+        batch_size=BATCH_SIZE,
+        validation_split=0.2,
+        subset='validation',
+        seed=SEED
+    )
+    # Set it up so that it runs faster while we need to access it
+    validation_dataset = raw_validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    print('Finished loading validation portion of the training data')
 
-print('Loading testing data')
-# Loading the testing dataset
-raw_test_dataset = tf.keras.utils.text_dataset_from_directory(
-    './dataset/aclImdb/test',
-    batch_size=BATCH_SIZE
-)
-# Set it up so that it runs faster while we need to access it
-test_dataset = raw_test_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    print('Loading testing data')
+    # Loading the testing dataset
+    raw_test_dataset = tf.keras.utils.text_dataset_from_directory(
+        './dataset/aclImdb/test',
+        batch_size=BATCH_SIZE
+    )
+    # Set it up so that it runs faster while we need to access it
+    test_dataset = raw_test_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    return train_dataset, validation_dataset, test_dataset, class_names
 
 if (MODE == 'view'):
+    train_dataset, validation_dataset, test_dataset, class_names = load_data()
     print('Viewing some data and how it is preprocessed.')
     # Take one batch of the training data
     for text_batch, label_batch in train_dataset.take(1):
@@ -71,6 +74,7 @@ if (MODE == 'view'):
     print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
 
 if (MODE == 'train'):
+    train_dataset, validation_dataset, test_dataset, class_names = load_data()
     print('Building model.')
     classifier_model = build_model()
     print(classifier_model.summary())
@@ -105,3 +109,18 @@ if (MODE == 'train'):
     # Save the model
     print(f'Saving the model to {MODEL_LOCATION}')
     classifier_model.save(MODEL_LOCATION, include_optimizer=False)
+
+if (MODE=='run'):
+    print('Running the model')
+    # Reload the model
+    classifier_model = tf.saved_model.load(MODEL_LOCATION)
+    while (True):
+        # Get user input
+        user_sentence = input('Write a sentence for the model to analyze!\nOtherwise type q to quit.\n')
+        # Proces input
+        if user_sentence == 'q':
+            break
+        # Get models prediction
+        sentiment_score = classifier_model([user_sentence])
+        # Print out the results (we know closer to 1 is a positive rating)
+        print(f'Input: {user_sentence} ---- Score: {sentiment_score[0][0]} ({"pos" if sentiment_score[0][0] > 0.5 else "neg"})')
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ SEED=7
 # 'view' shows you some of the data we are working with.
 # 'train' trains the model
 # 'run' runs the model on user input
-MODE='train'
+MODE='view'
 # URL to the BERT model and Preprocessing model you wish to use
 BERT_URL='https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
 PREPROCESSING_URL='https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
